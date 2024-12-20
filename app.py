52d59455d242a190f6e3608836692233411ca048
zachhco <zrocohen@gmail.com>
2024-12-20 15:10:52 -0800

    port

--- app.py
+++ app.py
@@ -47,4 +47,7 @@ def predict():
         return jsonify({'error': str(e)}), 400
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    # Retrieve the port number from the PORT environment variable
+    port = int(os.environ.get('PORT', 5000))  # Default to 5000 for local development
+    app.run(host='0.0.0.0', port=port)
